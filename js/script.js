09e06cd004fd5858d15403da982bf12e8040787d
Vladislav <dubovecvladislav0@gmail.com>
2023-04-06 17:59:44 +0300

    End room slider

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let myImageSlider = new Swiper('.slider__slider', {
+
+    navigation: {
+        prevEl: '.slider__button-prev',
+        nextEl: '.slider__button-next',
+    },
+
+    pagination: {
+        el: '.slider__pagination',
+        type: 'fraction',
+    },
+
+    grabCursor: true,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensitivity: 1,
+    },
+
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    speed: 1000,
+    autoHeight: false,
+
+    breakpoints: {
+        480: {
+            slidesPerView: 1,
+        },
+        992: {
+            slidesPerView: 2,
+        },
+    },
+
+});
